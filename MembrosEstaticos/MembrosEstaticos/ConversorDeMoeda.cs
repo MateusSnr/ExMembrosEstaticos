262dd375c5a783bc291e8bcb3d7a2a230a7bc118
Mateus Salles <devmateussnr@gmail.com>
2021-12-12 22:27:12 -0300

    Exercício Membros Estáticos

---
+++ MembrosEstaticos/MembrosEstaticos/ConversorDeMoeda.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MembrosEstaticos
+{
+    class ConversorDeMoeda
+    {
+        static public double CotacaoDolar;
+        static public double QtdASerComprada;
+
+        static public double ValorASerPagoEmReais()
+        {
+            return ((QtdASerComprada * CotacaoDolar)+((CotacaoDolar * QtdASerComprada) * 0.06));
+        }
+    }
+}
---
+++ MembrosEstaticos/MembrosEstaticos/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+
+namespace MembrosEstaticos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dólar: ");
+            ConversorDeMoeda.CotacaoDolar = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("\nQuantos dólares você vai comprar: ");
+            ConversorDeMoeda.QtdASerComprada = Convert.ToDouble(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("\nValor a ser pago em reais: " + ConversorDeMoeda.ValorASerPagoEmReais().ToString("F2", CultureInfo.InvariantCulture) + "\n");
+
+        }
+    }
+}
